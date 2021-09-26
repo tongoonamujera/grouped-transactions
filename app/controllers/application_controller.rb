d1a1705bd87ff9b14226408ab4d3bc4161a6d228
tongoonamujera <tongoonamujera@gmail.com>
2021-09-26 19:18:02 +0200

    Add username field to usermodel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_params, if: :devise_controller?:
+
+  def configure_params
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:username, :email, :password, :current_password)}
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def user_login_password
+    a = '2230959583'
+
+    a
+  end
 end
