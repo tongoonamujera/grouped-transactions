564baa5b2e4436995f3bd20a3c316367e1b0737f
tongoonamujera <tongoonamujera@gmail.com>
2021-09-28 06:12:56 +0200

    Add groups model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_params, if: :devise_controller?:
+  before_action :configure_params, if: :devise_controller?
 
   def configure_params
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password)}
