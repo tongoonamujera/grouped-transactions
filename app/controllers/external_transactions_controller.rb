60dbbb047672d88b15a151178bdc913dc88a7515
tongoonamujera <tongoonamujera@gmail.com>
2021-10-08 17:19:32 +0200

    Add more changes

--- app/controllers/external_transactions_controller.rb
+++ app/controllers/external_transactions_controller.rb
@@ -2,6 +2,6 @@ class ExternalTransactionsController < ApplicationController
   def index
     a = current_user.user_transactions.pluck(:id)
     b = GroupTransaction.where("user_transaction_id IN (?)", a).pluck(:user_transaction_id).uniq!
-    @external_transactions = current_user.user_transactions.where.not("id IN (?)", b)
+    @external_transactions = current_user.user_transactions.where.not("id IN (?)", b).order(created_at: :desc)
   end
 end
