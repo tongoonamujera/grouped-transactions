8e7101addda9b53ae0a198e5bafdabac638ddbf5
tongoonamujera <tongoonamujera@gmail.com>
2021-10-12 00:03:17 +0200

    Add home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :external_transactions
   resources :group_transactions do
     match '/new', to: 'group_transactions#create', via: :post, on: :collection
