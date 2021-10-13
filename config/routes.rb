59c23953af7fcb37150ab682e80ac882edc8efbd
tongoonamujera <tongoonamujera@gmail.com>
2021-10-13 22:33:47 +0200

    Change root_path

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   resources :groups
   devise_for :users
 
-  root 'groups#index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
