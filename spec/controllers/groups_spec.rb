2e1724e1a7dbf5d5b1ef1e830ef35ee559ce06e8
tongoonamujera <tongoonamujera@gmail.com>
2021-10-14 00:58:39 +0200

    Add tests for controllers

--- spec/controllers/groups_spec.rb
+++ spec/controllers/groups_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe 'Groups controller', type: :model do
   let(:second_group) { Group.create(name: 'second group', user_id: user1.id ) }
 
   describe 'GET index' do
-    it 'should show user with his id' do
+    it 'should show group with hts id' do
       group = Group.find(first_group.id)
       expect(group).to eq(first_group)
     end
 
-    it 'should show user with id 1 name' do
+    it 'should show group with its name' do
       second = Group.find(second_group.id).name
       expect(second).to eq('second group')
     end
--- spec/controllers/user_transactions_spec.rb
+++ spec/controllers/user_transactions_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe 'UserTransactions controller', type: :model do
   let(:second_transaction) { UserTransaction.create(name: 'tonaaaaaaaaaa', amount: 20, user_id: user1.id) }
 
   describe 'GET index' do
-    it 'should show user with his id' do
+    it 'should show user transaction with his id' do
       group = UserTransaction.find(first_transaction.id)
       expect(group).to eq(first_transaction)
     end
 
-    it 'should show user with id 1 name' do
+    it 'should show user transaction with  name' do
       second = UserTransaction.find(second_transaction.id).name
       expect(second).to eq('tonaaaaaaaaaa')
     end
