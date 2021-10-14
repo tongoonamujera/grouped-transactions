9d3a8f6b21bba3706cc67f34052227443b97c939
Tongoona Mujera <69446259+tongoonamujera@users.noreply.github.com>
2021-10-14 11:21:49 +0200

    Revert "Change root_path"

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   resources :groups
   devise_for :users
 
-  root 'home#index'
+  root 'groups#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
