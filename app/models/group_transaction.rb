cf3fa0a08355e1d9c5596086148ceb908d00ec87
Tongoona Mujera <69446259+tongoonamujera@users.noreply.github.com>
2021-10-16 01:12:30 +0200

    Update group_transaction.rb
    Remove unneccessary code in group transactions

--- app/models/group_transaction.rb
+++ app/models/group_transaction.rb
@@ -1,4 +1,4 @@
 class GroupTransaction < ApplicationRecord
-  belongs_to :group, dependent: :nullify
-  belongs_to :user_transaction, dependent: :nullify
+  belongs_to :group
+  belongs_to :user_transaction
 end
